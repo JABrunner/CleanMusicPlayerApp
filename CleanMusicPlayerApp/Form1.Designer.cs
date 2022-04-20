ae99dc243fb688c60f2e1017a8716a1c9e8bba27
laser <laser@XIAGAXPC>
2022-04-20 17:31:53 -0500

    Add project files.

---
+++ CleanMusicPlayerApp/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace CleanMusicPlayerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.TrackList = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.trackVolume = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(683, 45);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackgroundImage = global::CleanMusicPlayerApp.Properties.Resources.Close_48px;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(643, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(40, 39);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 377);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(657, 10);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ProgressBarMouseDown);
+            // 
+            // TrackList
+            // 
+            this.TrackList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TrackList.FormattingEnabled = true;
+            this.TrackList.Location = new System.Drawing.Point(450, 62);
+            this.TrackList.Name = "TrackList";
+            this.TrackList.Size = new System.Drawing.Size(159, 299);
+            this.TrackList.TabIndex = 4;
+            this.TrackList.SelectedIndexChanged += new System.EventHandler(this.TrackList_SelectedIndexChanged);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 62);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(432, 299);
+            this.axWindowsMediaPlayer1.TabIndex = 5;
+            // 
+            // trackVolume
+            // 
+            this.trackVolume.Location = new System.Drawing.Point(624, 62);
+            this.trackVolume.Maximum = 100;
+            this.trackVolume.Name = "trackVolume";
+            this.trackVolume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackVolume.Size = new System.Drawing.Size(45, 277);
+            this.trackVolume.TabIndex = 6;
+            this.trackVolume.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackVolume.Scroll += new System.EventHandler(this.trackVolume_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(615, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Volume";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackgroundImage = global::CleanMusicPlayerApp.Properties.Resources.opened_folder_100px;
+            this.btnOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnOpen.FlatAppearance.BorderSize = 0;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.Location = new System.Drawing.Point(408, 393);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(49, 38);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackgroundImage = global::CleanMusicPlayerApp.Properties.Resources.last_52px;
+            this.btnForward.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnForward.FlatAppearance.BorderSize = 0;
+            this.btnForward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForward.Location = new System.Drawing.Point(364, 393);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(38, 38);
+            this.btnForward.TabIndex = 1;
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackgroundImage = global::CleanMusicPlayerApp.Properties.Resources.stop_52px;
+            this.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Location = new System.Drawing.Point(320, 393);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(38, 38);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackgroundImage = global::CleanMusicPlayerApp.Properties.Resources.play_60px;
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Location = new System.Drawing.Point(276, 393);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(38, 38);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackgroundImage = global::CleanMusicPlayerApp.Properties.Resources.first_48px;
+            this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Location = new System.Drawing.Point(232, 393);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(38, 38);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(226, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "C# Clean Music Player";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(683, 454);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackVolume);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.TrackList);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox TrackList;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.TrackBar trackVolume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CleanMusicPlayerApp/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CleanMusicPlayerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*disables Windows Media Player controls to only show Visualizer, option in properties isn't working for
+            some reason*/
+            axWindowsMediaPlayer1.uiMode = "none";
+            trackVolume.Value = 50;
+        }
+        //create string to allow to select and load multiple files
+        string[] paths, files;
+        int flag = 1;
+        //create flag to allow toggle in Play/Pause button
+        Point lastPoint;
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Code to exit application
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Allow for selection of track in list and plays on click
+            axWindowsMediaPlayer1.URL = paths[TrackList.SelectedIndex];
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //Stop track
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            progressBar1.Value = 0;
+            //resets progress bar to zero when track is stopped
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //Toggle between play and pause and switch the image to denote which toggle is active
+            flag *= -1;
+            
+            if (flag == -1)
+            {
+                Bitmap b = new Bitmap(@"C:\Users\laser\OneDrive\Desktop\Coding Projects\C#\CleanMusicPlayerApp\CleanMusicPlayerApp\Resources\pause_48px.png");
+                btnPlay.BackgroundImage = b;
+            }
+            else
+            {
+                Bitmap b2 = new Bitmap(@"C:\Users\laser\OneDrive\Desktop\Coding Projects\C#\CleanMusicPlayerApp\CleanMusicPlayerApp\Resources\play_60px.png");
+                btnPlay.BackgroundImage = b2;
+            }
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                
+            }
+
+            else
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            //back up one track
+            if (TrackList.SelectedIndex > 0)
+            {
+                TrackList.SelectedIndex = TrackList.SelectedIndex - 1;
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            //move forward one track
+            if (TrackList.SelectedIndex<TrackList.Items.Count - 1)
+            {
+                TrackList.SelectedIndex = TrackList.SelectedIndex + 1;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //sync timer with progress bar to show how far into the track has played
+            if(axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                progressBar1.Maximum = (int)axWindowsMediaPlayer1.Ctlcontrols.currentItem.duration;
+                progressBar1.Value = (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            }
+        }
+
+        private void trackVolume_Scroll(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume = trackVolume.Value;
+        }
+
+        private void ProgressBarMouseDown(object sender, MouseEventArgs e)
+        {
+            //Allows progress bar to be clicked and move track relative to position of progress bar
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition = axWindowsMediaPlayer1.currentMedia.duration * e.X / progressBar1.Width;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //loads track into application opening File Explorer allowing use to pick file destination
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.FileNames;
+                paths = ofd.FileNames;
+                for (int x = 0; x < files.Length; x++)
+                {
+                    TrackList.Items.Add(files[x]);
+                }
+            }
+        }
+    }
+}
+
+
+
+//Created by Jordan Brunner
